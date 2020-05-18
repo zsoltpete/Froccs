a3debf9daba484d7db7de82d384258e42b82ec2d
pete.zsolt <pete.zsolt@codeyard.eu>
2020-05-18 23:44:49 +0200

    ✨ Tests modified

--- FroccsTests/FroccsTests.swift
+++ FroccsTests/FroccsTests.swift
@@ -9,13 +9,13 @@
 import XCTest
 
 class FroccsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    override func tearDown() {
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override class func setUp() {
+        
     }
 
     func testExample() throws {
